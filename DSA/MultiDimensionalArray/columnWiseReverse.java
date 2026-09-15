b5b664960fc4389f0a9d59e24f4c767dbf54955e
hritik12345444 <hritik12345444@gmail.com>
2026-09-15 23:49:40 +0530

    add column wise traversal

--- DSA/MultiDimensionalArray/columnWiseReverse.java
+++ DSA/MultiDimensionalArray/columnWiseReverse.java
@@ -22,6 +22,14 @@ public class columnWiseReverse {
             end--;
         }
 
+        // column wise traverse 
+        for(int j=0;j<a[0].length; j++){
+            for(int i=0;i<a.length; i++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
         for(int i=0;i<a.length;i++){
             for(int j=0;j<a[i].length;j++){
                 System.out.print(a[i][j] + " ");
